533ff0663486e5322ae001983fb4e1dc0fa25e2f
SuleMareVientu <66548690+SuleMareVientu@users.noreply.github.com>
2023-08-06 18:53:47 +0200

    Implemented a new and more reliable method

--- NoWrithe/script.cpp
+++ NoWrithe/script.cpp
@@ -1,23 +1,47 @@
 #include "script.h"
 
-int main() {
+DWORD	pedUpdateTime;
 
-	while (true)
+void update()
+{
+	Player player = PLAYER::PLAYER_ID();
+	Ped playerPed = PLAYER::PLAYER_PED_ID();
+
+	// check if player ped exists and control is on (e.g. not in a cutscene)
+	if (!ENTITY::DOES_ENTITY_EXIST(playerPed) || !PLAYER::IS_PLAYER_CONTROL_ON(player))
+		return;
+
+	// get all peds
+	const int ARR_SIZE = 1024;
+	Ped peds[ARR_SIZE];
+	int count = worldGetAllPeds(peds, ARR_SIZE);
+
+	// set PED_FLAG_NO_WRITHE 2 times a second
+	if (pedUpdateTime + 500 < GetTickCount())
 	{
-		Player player = PLAYER::PLAYER_ID();
-		Ped playerPed = PLAYER::PLAYER_PED_ID();
-		Entity target;
-		if (ENTITY::DOES_ENTITY_EXIST(playerPed))
-			if (PLAYER::GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(player, &target) || PLAYER::GET_PLAYER_TARGET_ENTITY(player, &target))
-				if (ENTITY::IS_ENTITY_A_PED(target))
-					PED::SET_PED_CONFIG_FLAG(target, 281, 1);
-		WAIT(0);
+		pedUpdateTime = GetTickCount();
+		for (int i = 0; i < count; i++)
+		{
+			if (peds[i] != playerPed)
+			{
+				PED::SET_PED_CONFIG_FLAG(peds[i], 281, 1);
+			}
+		}
 	}
-
-	return 0;
+	return;
 }
 
-void ScriptMain() {
+void main()
+{
+	while (true)
+	{
+		update();
+		WAIT(0);
+	}
+}
+
+void ScriptMain()
+{
 	srand(GetTickCount());
-	throw main();
+	main();
 }
